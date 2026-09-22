a520dbf8d3503d3cdced9925b5de564665a9a8c0
sama233 <97009482+casama233@users.noreply.github.com>
2026-09-22 20:32:14 +0800

    fix: canonicalize A2.7.58 worldgen fruiting bridge leaves

---
+++ development/gameplay_core/a2758_pepper_worldgen_fruiting_runtime.js
@@ -0,0 +1,20 @@
+import {system,BlockPermutation} from '@minecraft/server';
+import {
+ PEPPER_LEAVES_ID,PEPPER_WORLDGEN_FRUITING_BRIDGE_ID,
+ PEPPER_WORLDGEN_FRUITING_COMPONENT_ID,PEPPER_HAS_STATE,PEPPER_PERSISTENT_STATE
+} from './a2758_pepper_worldgen_fruiting_core.js';
+
+system.beforeEvents.startup.subscribe(event=>{
+ event.blockComponentRegistry.registerCustomComponent(PEPPER_WORLDGEN_FRUITING_COMPONENT_ID,{
+  onTick(e){
+   const block=e.block;
+   if(block?.typeId!==PEPPER_WORLDGEN_FRUITING_BRIDGE_ID)return;
+   try{
+    block.setPermutation(BlockPermutation.resolve(PEPPER_LEAVES_ID,{
+     [PEPPER_HAS_STATE]:true,
+     [PEPPER_PERSISTENT_STATE]:false
+    }));
+   }catch{}
+  }
+ });
+});
